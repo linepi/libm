18eb47c6aca3d9932faed26a88a10fb048da7df7
linepi <2428513224@qq.com>
2023-12-18 14:39:53 +0800

    impl path3

--- impl_ccosh.c
+++ impl_ccosh.c
@@ -6,33 +6,27 @@
 #include <stdlib.h>
 
 #define DLL_FILE_NAME "/usr/lib64/libimf.so"
-void print_every_byte(void *vid, int len)
-{
-	uint8_t *p = (uint8_t *)vid;
-	for (int i = len - 1; i >= 0; i--) { // 以下是逐byte打印
-			uint8_t byte = p[i];
-			printf("%02x ", byte);
-	}
-	printf("\n");
-}
-
 union doubleu {
 	double f;
 	uint64_t i;
 };
+
+#define DOUBLE_UINT64(d) (*(uint64 *)&d)
+#define DOUBLE_EXP(d) (DOUBLE_UINT64(d) >> 52U)
+#define DOUBLE_LOW32(d) (DOUBLE_UINT64(d) & 0xffffffff)
+#define DOUBLE_32to51(d) ((DOUBLE_UINT64(d) >> (32U)) & 0xfffff)
 
 // rip + 0x1c40cd
 uint8_t ccosh_global[] = {
 	0x00,   0x00,   0x00,   0x00,   0x00,   0x00,   0x10,   0x00,
 	0x00,   0x00,   0x00,   0x00,   0x00,   0x00,   0xf0,   0x4f
 };
-
 double ccosh_const0 = *(double *)(ccosh_global + 2); // 134217729
 double ccosh_const1 = *(double *)(ccosh_global); // ?
 double ccosh_const2 = (double)0x7FE0000000000000;
 
 static inline void ccosh_path1(double x, double y, double *res_x, double *res_y) {
-	int y_nan_infinite = ((*(uint64 *)&y) >> 52U) == 0x7ff;   
+	int y_nan_infinite = DOUBLE_EXP(y) == 0x7ff;   
 	double t1, t2;
 	t1 = ccosh_const2 * ccosh_const2;	
 	t2 = cos(y) * t1;
@@ -127,15 +121,47 @@ static void ccosh_path2(double x, double y, double *res_x, double *res_y) {
 }
 
 static void ccosh_path3(int cos, double x, double y, double *res_x, double *res_y) {
-	// stack variable
-	double cosh_res[2]; // off 0x0
-	double sinh_res[2]; // off 0x10
-	double sin_y, cos_y; // off 0x30, 0x38
+	int x_nan_infinite = DOUBLE_EXP(x) == 0x7ff;
+	int con1 = DOUBLE_32to51(x) != 0;
+	int con2 = DOUBLE_LOW32(x) != 0; 
+	int con3 = DOUBLE_32to51(y) != 0;
+	int con4 = DOUBLE_LOW32(y) != 0;
+	double t1, t2;
+	if (cos) {
+		t1 = cos(y);
+	} else {
+		t1 = cosh(x);
+	}
 
+	if (x_nan_infinite && !con1 && !con2) {
+		if (!con3 && !con4) {
+			uint8_t al, dl;
+			dl = DOUBLE_UINT64(x) & 0xff;
+			al = DOUBLE_UINT64(y) & 0xff;
+			dl &= 0x80;
+			al &= 0x80;
+			dl >>= 7;
+			al >>= 7;
+			dl ^= al;
+
+			double t0 = 0.0;
+			if (dl == 0) {
+				t2 *= t0 * 1.0;
+			} else {
+				t2 *= t0 * -1.0;
+			}
+		} else {
+			t2 = 0;
+		}
+	} else {
+		t2 = sin(y);
+	}
+	*res_x = t1;
+	*res_y = t2;
 }
 
 static inline void ccosh_path4(double x, double y, double *res_x, double *res_y) {
-	int y_nan_infinite = ((*(uint64 *)&y) >> 52U) == 0x7ff;   
+	int y_nan_infinite = DOUBLE_EXP(y) == 0x7ff;   
 	double t1, t2;
 	t1 = cosh(x) * cos(y);
 	if (y_nan_infinite) {
@@ -148,17 +174,17 @@ static inline void ccosh_path4(double x, double y, double *res_x, double *res_y)
 }
 
 static void ccosh_path5(double x, double y, double *res_x, double *res_y) {
-	int x_nan_infinite = ((*(uint64 *)&x) >> 52U) == 0x7ff;   
-	int con1 = ((x >> (32U)) & 0xfffff) != 0;
-	int con2 = (x & 0xffffffff) != 0;
+	int x_nan_infinite = DOUBLE_EXP(x) == 0x7ff;   
+	int con1 = DOUBLE_32to51(x) != 0;
+	int con2 = DOUBLE_LOW32(x) != 0;
 
 	double t1, t2;
 	t1 = cosh(x);
 	t2 = y;
 	if (!x_nan_infinite || (x_nan_infinite && !con1 && !con2)) {
 		uint8_t al, dl, cl;
-		dl = t2 & 0xff; 
-		al = t1 & 0xff;
+		dl = DOUBLE_UINT64(t2) & 0xff; 
+		al = DOUBLE_UINT64(t1) & 0xff;
 		cl = dl;
 		cl &= 0x7f;
 		dl >>= 7;	
@@ -177,28 +203,24 @@ static void ccosh_path5(double x, double y, double *res_x, double *res_y) {
 double complex _ccosh(double complex in) 
 {
 	double res_x, res_y; // off 0x40, 0x48
-
 	double x = creal(in); // off 0x50
 	double y = cimag(in); // off 0x58
-	union doubleu x_u;	
-	x_u.f = x;
-	union doubleu y_u;	
-	y_u.f = y;
-	uint64_t x_exp = (x_u.i >> 52U) & 0x7ff;   // exponent
-	uint64_t y_exp = (y_u.i >> 52U) & 0x7ff;   
+
+	uint64_t x_exp = DOUBLE_EXP(x);   // exponent
+	uint64_t y_exp = DOUBLE_EXP(y);   
 
 	int con0 = x_exp == 0x7ff;
 	int con1 = x_exp == 0;
-	int con2 = ((x >> (32U)) & 0xfffff) != 0;
-	int con3 = (x & 0xffffffff) != 0;
+	int con2 = DOUBLE_32to51(x) != 0;
+	int con3 = DOUBLE_LOW32(x) != 0;
 	int con9 = x_exp < 0x40a;
 
 	int con4 = y_exp == 0;
 	int con5 = y_exp != 0;
 	int con6 = y_exp == 0x7ff;
 	int con7 = y_exp != 0x7ff;
-	int con8 = (y & 0xffffffff) != 0;
-	int con10 = ((y >> (32U)) & 0xfffff) != 0;
+	int con8 = DOUBLE_LOW32(y) != 0;
+	int con10 = DOUBLE_32to51(y) != 0;
 
 	if (!con0) {
 		if (con1 && !con2 && !con3) {
